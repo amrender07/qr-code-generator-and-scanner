61a1ae62957c04164786c6c276ad280653b1f2d5
AMRENDER SINGH <118029886+amrender07@users.noreply.github.com>
2023-08-31 07:28:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React, {useState, useRef} from 'react';
+import {Container, Card, CardContent, makeStyles, Grid, TextField, Button} from '@material-ui/core';
+import QRCode from 'qrcode';
+import QrReader from 'react-qr-reader';
+
+
+function App() { 
+  const [text, setText] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [setScanResultFile] = useState('');
+  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const classes = useStyles();
+  const qrRef = useRef(null);
+
+
+  
+  const generateQrCode = async () => {
+    try {
+          const response = await QRCode.toDataURL(text);
+          setImageUrl(response);
+    }catch (error) {
+      console.log(error);
+    }
+  }
+  const handleErrorFile = (error) => {
+    console.log(error);
+  }
+  const handleScanFile = (result) => {
+      if (result) {
+          setScanResultFile(result);
+      }
+  }
+  const onScanFile = () => {
+    qrRef.current.openImageDialog();
+  }
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  }
+  const handleScanWebCam = (result) => {
+    if (result){
+      setScanResultWebCam(result);}}
+  
+      const renderScannedLink = (text) => {
+        if ((text)) {
+          return (
+            <a href={text} target="_blank" rel="noopener noreferrer">
+              Open Link
+            </a>
+          );
+        } else {
+          return text;
+        }
+      }
+      
+  return (
+    <Container className={classes.conatiner}>
+          <Card>
+              <h2 className={classes.title}>Generate Download & Scan QR Code</h2>
+              <CardContent>
+                  <Grid container spacing={2}>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                          <TextField label="Enter Text Here" onChange={(e) => setText(e.target.value)}/>
+                          <Button className={classes.btn} variant="contained" 
+                            color="primary" onClick={() => generateQrCode()}>Generate</Button>
+                            <br/>
+                            <br/>
+                            <br/>
+                            {imageUrl ? (
+                              <a href={imageUrl} download>
+                                  <img src={imageUrl} alt="img"/>
+                              </a>) : null}
+                      </Grid>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                        <Button className={classes.btn} variant="contained" color="secondary" onClick={onScanFile}>Scan Qr Code</Button>
+                        <QrReader
+                          ref={qrRef}
+                          delay={300}
+                          style={{width: '100%'}}
+                          onError={handleErrorFile}
+                          onScan={handleScanFile}
+                          legacyMode
+                        />
+                        <h3>Scanned Code: {renderScannedLink(scanResultWebCam)}</h3>
+                      </Grid>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                         <h3>Qr Code Scan by Web Cam</h3>
+                         <QrReader
+                         delay={200}
+                         style={{width: '100%'}}
+                         onError={handleErrorWebCam}
+                         onScan={handleScanWebCam}
+                         />
+                         <h3>Scanned By WebCam Code: {renderScannedLink(scanResultWebCam)}</h3>
+                      </Grid>
+                  </Grid>
+              </CardContent>
+          </Card>
+    </Container>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+    conatiner: {
+      marginTop: 20
+    },
+    title: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems:  'center',
+      background: '#3f51b5',
+      color: '#rrr',
+      padding: 20
+    },
+    btn : {
+      marginTop: 10,
+      marginBottom: 20
+    }
+}));
+export default App;
